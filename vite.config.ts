dbb44e2778d55576675719dade3aad4a8f7af614
KazuhiroIto0127 <kazuhiroito0127@gmail.com>
2025-05-03 22:14:44 +0900

    install tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => ({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   base: command === 'build' ? '/learn_gsap/' : '/', // ビルド時のみGitHub Pages用のパスを設定
 }))
