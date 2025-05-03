a0a357ad6ea6e1b0d45f6e8caee7127209eab20d
KazuhiroIto0127 <kazuhiroito0127@gmail.com>
2025-05-03 23:25:02 +0900

    animate rotation box and circle by useGSAP

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,35 @@
-import { useEffect, useRef } from 'react'
-import { gsap } from 'gsap'
-import './App.css'
+import { useRef } from 'react'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
+
+gsap.registerPlugin(useGSAP);
 
 function App() {
-  const helloRef = useRef<HTMLDivElement>(null)
+  const container = useRef();
+  const circle = useRef();
+  const box = useRef();
 
-  useEffect(() => {
-    if (helloRef.current) {
-      // 初期状態を設定
-      gsap.set(helloRef.current, {
-        opacity: 0,
-        y: -100
-      })
-
-      // アニメーションを実行
-      gsap.to(helloRef.current, {
-        duration: 2,
-        y: 0,
-        opacity: 1,
-        rotation: 360,
-        ease: "bounce.out",
-        onComplete: () => {
-          console.log('アニメーション完了！')
-        }
-      })
-    }
-  }, [])
+  useGSAP(
+    () => {
+      gsap.to(box.current, { rotation: "+=360", duration: 3 });
+      gsap.to(circle.current, { rotation: "-=360", duration: 3 });
+    },
+    { scope: container }
+  );
 
   return (
-    <div className="app">
-      <div ref={helloRef} className="hello-world">
-        Hello World
+    <>
+      <div ref={container} className="w-full h-full pt-5 px-6">
+        <div className="flex items-center justify-center w-32 h-32 bg-gradient-to-r from-blue-500 to-green-500 rounded-2xl"
+             ref={box}>
+              selector
+        </div>
+        <div className="flex items-center justify-center w-32 h-32 bg-gradient-to-b rounded-full from-yellow-400 to-red-500"
+              ref={circle}>
+          Circle
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 
